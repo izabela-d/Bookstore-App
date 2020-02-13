33ffdd9e1d5a38441968d64b542254e791fc4081
Izabela Depa <izabela1depa@gmail.com>
2020-02-14 00:07:23 +0100

    Cleanup pagination

--- client/src/components/common/Pagination/Pagination.js
+++ client/src/components/common/Pagination/Pagination.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 import './Pagination.scss';
