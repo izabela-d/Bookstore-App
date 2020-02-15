6b67b5dd0f78ba3b852bd4915fccfe9bb2302a41
Izabela Depa <izabela1depa@gmail.com>
2020-02-16 00:23:34 +0100

    Add service worker

--- server/config.js
+++ server/config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    PORT: 8000,
-    DB: 'mongodb://localhost:27017/FinallyApp',
+    PORT: process.env.PORT || 8000,
+    DB: (process.env.NODE_ENV === 'production') ? 'mongodb+srv://project-0-vnvca.mongodb.net/bookstoreapp?retryWrites=true&w=majority' : 'mongodb://localhost:27017/FinallyApp',
+    MONGO_USER: (process.env.NODE_ENV === 'production') ? process.env.MONGO_USER : '',
+    MONGO_PASS: (process.env.NODE_ENV === 'production') ? process.env.MONGO_PASS : ''
 };
